318bb30235b6f75ed9f3ea19a8f5d2f258621028
Mark Reeves <mjr@clearbold.com>
2021-09-11 23:01:54 -0400

    work page

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -3,14 +3,203 @@
     <div class="wrapper panel panel-alternating bg-solid">
       <h1>Work Highlights</h1>
     </div>
-    <div class="panel panel-alternating">
-      <div class="wrapper">
+    <div class="panel panel-work work-morins">
+      <div
+        class="image image-logo"
+        :style="{
+          backgroundImage: 'url(' + require('@/assets/img/morins-bg.jpg') + ')',
+        }"
+      >
+        <g-image
+          class="logo-morins"
+          src="~/assets/img/logo-morins.png"
+          alt=""
+          width="290"
+          height="277"
+        />
+      </div>
+      <div class="text">
+        <div class="wrapper">
+          <h2>Russell Morin Catering &amp; Events</h2>
+          <p>
+            Mark has worked with
+            <a target="_blank" href="https://morins.com">Morin&rsquo;s</a> for
+            over a decade, including two site redesigns&mdash;first on
+            ExpressionEngine and then Craft CMS&mdash;and the recent launch of
+            <a target="_blank" href="https://orderfresheats.com">Fresh Eats</a>,
+            a Craft Commerce site focused on corporate &amp; casual catering.
+            Along the way he&rsquo;s also supported CRM explorations, marketing
+            automation, lead capture, and API&nbsp;integrations.
+          </p>
+          <p>
+            Morin&rsquo;s is a distinctive brand delivering distinctive cuisine
+            and memorable events to their clientele. Mark&rsquo;s partnership
+            with Morin&rsquo;s is built on ensuring that their website and
+            marketing efforts reflect their high standards, and&nbsp;his.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-work work-jn">
+      <div
+        class="image"
+        :style="{
+          backgroundImage:
+            'url(' + require('@/assets/img/monarch-bg.jpg') + ')',
+        }"
+      >
+        <g-image
+          class="photo-jn"
+          src="~/assets/img/monarch.jpg"
+          alt=""
+          width="1152"
+          height="664"
+        />
+      </div>
+      <div class="text">
+        <div class="wrapper">
+          <h2>Journey North</h2>
+          <p>
+            Mark worked directly with Journey North Founder Elizabeth Howard to
+            establish a new Drupal website for editorial content &amp;
+            workflows. Following that effort, he engaged in a support role for
+            legacy site content, the legacy citizen science reporting platform,
+            and the new Drupal&nbsp;website.
+          </p>
+          <p>
+            He also designed and built a new maps experience using Leaflet.js
+            and Esri base layers; identified reliable service providers and
+            established sustainable budgets for infrastructure &amp; support;
+            migrated all digital properties to new hosting environments twice;
+            updated website security protocols to support a community of 100,000
+            users; and updated PHP code to modern best&nbsp;practices.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-work work-idg">
+      <div class="image">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 314 54.3"
+          class="c-page-header__logo"
+        >
+          <title>logo</title>
+          <path
+            d="M51.3,52.2H40.9V1.8H51.3Z"
+            class="c-page-header__logo-bar c-page-header__logo-bar--alt"
+          ></path>
+          <path
+            d="M32.2,52.5H21.7V15.4H32.2Z"
+            class="c-page-header__logo-bar"
+          ></path>
+          <path
+            d="M13,52.5H2.5V27.9H13Z"
+            class="c-page-header__logo-bar"
+          ></path>
+          <g class="c-page-header__logo-text">
+            <path
+              d="M73.4,38.7a.8.8,0,0,1-.8-.8h0V22a.8.8,0,0,1,1.6,0V38A.8.8,0,0,1,73.4,38.7Z"
+            ></path>
+            <path
+              d="M93.4,38.7a.7.7,0,0,1-.6-.3h-.1l-10-13.9V38a.8.8,0,0,1-1.5,0V22.3a.77.77,0,0,1,1.4-.6l10,13.9V22.2a.8.8,0,0,1,1.6,0V38A.8.8,0,0,1,93.4,38.7Z"
+            ></path>
+            <path
+              d="M107.6,38.7h-.4l-.3-.3h-.1l-6.6-15.8a.76.76,0,0,1,.4-1h.6a.8.8,0,0,1,.4.4l6,14.2,6-14.2a.7.7,0,0,1,.4-.4h.6a.7.7,0,0,1,.4.4.8.8,0,0,1,0,.6l-6.6,15.6a.7.7,0,0,1-.5.6h-.3Z"
+            ></path>
+            <path
+              d="M129.6,38.7h-8.9a.8.8,0,0,1-.7-.8V22a.8.8,0,0,1,.8-.8h8.8a.8.8,0,1,1,0,1.6h-8v6.4h7a.8.8,0,1,1,0,1.6h-7v6.4h8a.8.8,0,0,1,0,1.6Z"
+            ></path>
+            <path
+              d="M141.6,38.8a11,11,0,0,1-6.4-2.5.7.7,0,0,1-.11-1v0h0l.5-.3.5.2a9.5,9.5,0,0,0,5.4,2.2,6.4,6.4,0,0,0,3.7-1.1,3,3,0,0,0,1.4-2.4,2.5,2.5,0,0,0-1.3-2.2,11,11,0,0,0-3.9-1.4h0a10.9,10.9,0,0,1-4.1-1.5,3.7,3.7,0,0,1-1.8-3.1,4.1,4.1,0,0,1,1.9-3.4,7.2,7.2,0,0,1,4.2-1.3,9.6,9.6,0,0,1,5.4,2,.7.7,0,0,1,.22,1l0,0-.4.3h-.5a8.4,8.4,0,0,0-4.6-1.8,6,6,0,0,0-3.4,1,2.8,2.8,0,0,0-1.3,2.2,2.3,2.3,0,0,0,1.2,1.9,9.8,9.8,0,0,0,3.6,1.3h0a11.7,11.7,0,0,1,4.5,1.6,4,4,0,0,1,2,3.4,4.5,4.5,0,0,1-2,3.6A7.7,7.7,0,0,1,141.6,38.8Z"
+            ></path>
+            <path
+              d="M158.2,38.7a.8.8,0,0,1-.8-.8h0V22.8h-5a.8.8,0,1,1,0-1.6h11.5a.8.8,0,1,1,0,1.6h-5V38A.8.8,0,0,1,158.2,38.7Z"
+            ></path>
+            <path
+              d="M179.6,38.7h-8.9a.8.8,0,0,1-.7-.8V22a.8.8,0,0,1,.8-.8h8.8a.8.8,0,1,1,0,1.6h-8v6.4h7a.8.8,0,1,1,0,1.6h-7v6.4h8a.8.8,0,0,1,0,1.6Z"
+            ></path>
+            <path
+              d="M189.8,38.7h-3.3a.8.8,0,0,1-.7-.8V22a.8.8,0,0,1,.8-.8h3.2a8.75,8.75,0,0,1,0,17.5Zm-2.4-1.6h2.4a7.2,7.2,0,0,0,0-14.4h-2.4Z"
+            ></path>
+            <path
+              d="M217.2,38.7h-3.3a.8.8,0,0,1-.7-.8V22a.8.8,0,0,1,.8-.8h3.2a8.75,8.75,0,0,1,0,17.5Zm-2.4-1.6h2.4a7.2,7.2,0,0,0,0-14.4h-2.4Z"
+            ></path>
+            <path
+              d="M231.7,38.7a.8.8,0,0,1-.8-.8h0V22a.8.8,0,1,1,1.6,0h0V38A.8.8,0,0,1,231.7,38.7Z"
+            ></path>
+            <path
+              d="M247.8,38.7a8.9,8.9,0,1,1,0-17.7,9.2,9.2,0,0,1,5.7,2,.8.8,0,0,1,.3.5.7.7,0,0,1-.2.6.8.8,0,0,1-1.1.1,7.34,7.34,0,1,0-4.7,13,7.6,7.6,0,0,0,4.4-1.4V31h-4.5a.8.8,0,0,1-.8-.8h0a.8.8,0,0,1,.8-.8H253a.8.8,0,0,1,.7.8v6a.9.9,0,0,1-.3.6A9,9,0,0,1,247.8,38.7Z"
+            ></path>
+            <path
+              d="M261.6,38.7a.8.8,0,0,1-.8-.8V22a.8.8,0,1,1,1.6,0h0V38A.8.8,0,0,1,261.6,38.7Z"
+            ></path>
+            <path
+              d="M274.5,38.7a.8.8,0,0,1-.8-.8V22.8h-5a.8.8,0,0,1,0-1.6h11.5a.8.8,0,0,1,0,1.6h-5V38A.8.8,0,0,1,274.5,38.7Z"
+            ></path>
+            <path
+              d="M295.7,38.7a.8.8,0,0,1-.7-.5l-1.5-3.9h-7.6l-1.5,3.9a.8.8,0,0,1-.7.5h-.3a.8.8,0,0,1-.4-.4.7.7,0,0,1,0-.6l6-15.9a.89.89,0,0,1,1.6,0l5.9,15.9a.8.8,0,0,1-.4,1h-.4Zm-9.2-6h6.4l-3.2-8.5Z"
+            ></path>
+            <path
+              d="M310.7,38.7h-8.8a.8.8,0,0,1-.8-.8V22a.8.8,0,1,1,1.6,0h0V37.2h8a.8.8,0,0,1,0,1.6h0Z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div class="text">
+        <div class="wrapper">
+          <h2>Invested Digital Group</h2>
+          <p>
+            Mark has fully developed, launched, and supported multiple Craft CMS
+            websites for IDG&rsquo;s Biosciences&nbsp;clients.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="other-clients wrapper panel panel-alternating bg-solid">
+      <div class="panel-intro">
+        <h2>Other Clients</h2>
+      </div>
+      <div>
         <p>
-          Include Morin's, Journey North, Sentinel Benefits, Invested Digital
-          Group
+          Mark coded front end templates &amp; CSS for integration in
+          <a target="_blank" href="https://sentinelbenefits.com"
+            >Sentinel Benefits&rsquo;</a
+          >
+          Kentico&nbsp;website.
+        </p>
+        <p>
+          He&rsquo;s also managed or supported work for ESPN,
+          Fidelity&nbsp;Investments, The University of Wisconsin at Madison,
+          History at MIT, MIT Hyperstudio, The Digital Research Center at
+          Hofstra&nbsp;University,
+          <a target="_blank" href="https://vascore.org">VasCore</a>, Farm
+          Collective&nbsp;Wine, and&nbsp;40Act.com.
         </p>
       </div>
     </div>
+    <!-- <div class="panel panel-work work-sentinel">
+      <div class="image image-logo">
+        <g-image
+          class="logo-sentinel"
+          src="~/assets/img/logo-sentinel.png"
+          alt=""
+          width="406"
+          height="168"
+        />
+      </div>
+      <div class="text">
+        <div class="wrapper">
+          <h2>Sentinel Benefits</h2>
+          <p>
+            Mark has supported front end web development for two
+            <a target="_blank" href="https://sentinelbenefits.com">Sentinel</a>
+            website redesigns, overseeing efforts once, and personally coding
+            templates ready for integration in Sitecore the second time.
+          </p>
+        </div>
+      </div>
+    </div> -->
     <div class="quote wrapper panel panel-alternating">
       <blockquote>
         <p>
@@ -45,3 +234,116 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import "~/assets/css/mixins";
+@import "~/assets/css/vars";
+.panel-work {
+  .image {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    @include remify("min-height", 200px);
+    &.image-logo {
+      @include remify("padding-right", 40px);
+      @include remify("padding-left", 40px);
+    }
+  }
+  .text {
+    /* background-color: var(--color-gray); */
+    @include remify("padding-top", 40px);
+    @include remify("padding-bottom", 40px);
+  }
+  @media all and (min-width: $breakpoint-min) {
+    align-items: stretch;
+    display: flex;
+    @include remify("min-height", 400px);
+    & > div {
+      flex-basis: 50%;
+    }
+    .text {
+      align-items: center;
+      display: flex;
+    }
+  }
+}
+.work-jn,
+.work-sentinel {
+  @media all and (min-width: $breakpoint-min) {
+    flex-direction: row-reverse;
+  }
+}
+.work-morins,
+.work-sentinel,
+.work-idg {
+  @media all and (max-width: $breakpoint-max) {
+    .image {
+      @include remify("padding-top", 40px);
+      @include remify("padding-bottom", 40px);
+    }
+  }
+}
+.work-morins {
+  @media all and (min-width: $breakpoint-min) {
+    .text {
+      background-color: var(--color-cream);
+    }
+  }
+}
+.work-idg {
+  .image {
+    background-color: #27abeb;
+    svg {
+      @include remify("max-width", 240px);
+    }
+    .c-page-header__logo-bar--alt,
+    .c-page-header__logo-text {
+      fill: white;
+    }
+  }
+  /* .text {
+    background-color: var(--color-cream);
+  } */
+}
+.work-morins {
+  .image {
+    .logo-morins {
+      @include remify("max-width", 180px);
+    }
+  }
+}
+.work-sentinel {
+  .image {
+    .logo-sentinel {
+      @include remify("max-width", 203px);
+    }
+    @media all and (min-width: $breakpoint-min) {
+      background-color: var(--color-gray);
+    }
+  }
+}
+.work-morins,
+.work-jn {
+  .image {
+    background-position: center center;
+    background-size: cover;
+  }
+}
+.work-jn {
+  @media all and (min-width: $breakpoint-min) {
+    .image {
+      .photo-jn {
+        display: none;
+      }
+    }
+  }
+}
+.other-clients {
+  div:not(.panel-intro) {
+    margin-right: auto;
+    margin-left: auto;
+    @include remify("max-width", 800px);
+    text-align: center;
+  }
+}
+</style>
