08d1d319bea7ce85aaf4161e9d489b4a6e2917d9
Mark Reeves <mjr@clearbold.com>
2021-10-03 08:34:43 -0400

    Removed SVG alt attributes

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -66,7 +66,6 @@
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 314 54.3"
           class="c-page-header__logo"
-          alt=""
         >
           <title>logo</title>
           <path
